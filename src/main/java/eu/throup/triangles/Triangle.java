357963abe8534a34ef09ebb7599f441156dc4e97
Chris Throup <chris@throup.eu>
2017-02-07 11:23:00 +0000

    Expand impossible triangle tests to cover all possible largest sides.

--- src/main/java/eu/throup/triangles/Triangle.java
+++ src/main/java/eu/throup/triangles/Triangle.java
@@ -14,7 +14,7 @@ public class Triangle {
         this.side2 = side2;
         this.side3 = side3;
 
-        if (side1 + side2 < side3) {
+        if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/eu/throup/triangles/ImpossibleTriangleTest.java
+++ src/test/java/eu/throup/triangles/ImpossibleTriangleTest.java
@@ -3,15 +3,35 @@ package eu.throup.triangles;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ImpossibleTriangleTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+    @Parameterized.Parameter(0)
+    public double side1;
+    @Parameterized.Parameter(1)
+    public double side2;
+    @Parameterized.Parameter(2)
+    public double side3;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, 1, 3},
+                {1, 3, 1},
+                {3, 1, 1},
+        });
+    }
 
     @Test
     public void test() {
         thrown.expect(IllegalArgumentException.class);
-
-        new Triangle(1, 1, 3);
+        new Triangle(side1, side2, side3);
     }
 }
