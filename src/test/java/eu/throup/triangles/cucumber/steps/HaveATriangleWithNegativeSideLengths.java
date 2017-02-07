7987b86a0006d35901853a7e4082473af09da715
Chris Throup <chris@throup.eu>
2017-02-07 12:50:18 +0000

    Negative sides feature is now implemented and passing with no code changes.

--- src/test/java/eu/throup/triangles/cucumber/steps/HaveATriangleWithNegativeSideLengths.java
+++ src/test/java/eu/throup/triangles/cucumber/steps/HaveATriangleWithNegativeSideLengths.java
@@ -1,14 +1,17 @@
 package eu.throup.triangles.cucumber.steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
+import eu.throup.triangles.Triangle;
 import eu.throup.triangles.cucumber.TestHelper;
 
 public class HaveATriangleWithNegativeSideLengths implements En {
     public HaveATriangleWithNegativeSideLengths(TestHelper helper) {
         Given("^I have a triangle with negative side lengths$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            try {
+                helper.triangle = new Triangle(-1, -1, -1);
+            } catch (Exception e) {
+                helper.exception = e;
+            }
         });
     }
 }
